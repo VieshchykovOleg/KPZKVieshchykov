9ed61d42d44753667b9fc3dec96cd89093557ca2
olegveshchykov <ipz222_vom@student.ztu.edu.ua>
2024-05-19 16:46:26 +0300

    тест в головному файлі  класу EditingEditing

--- Notepad/Form1.cs
+++ Notepad/Form1.cs
@@ -11,11 +11,14 @@ namespace Notepad
     public partial class Form1 : Form
     {
         private FileManager _fileManager;
+        private TextEditor _textEditor;
+
 
         public Form1()
         {
             InitializeComponent();
             _fileManager = new FileManager(richTextBox1);
+            _textEditor = new TextEditor(richTextBox1);
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -69,6 +72,66 @@ namespace Notepad
         {
             Form1 newForm = new Form1();
             newForm.Show();
+        }
+        // Використання класу Editing
+        private void UndoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _textEditor.Undo();
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _textEditor.Cut();
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _textEditor.Copy();
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _textEditor.Paste();
+        }
+
+        private void DeleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _textEditor.Delete();
+        }
+
+        private void SelectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _textEditor.SelectAll();
+        }
+
+        private void ReplaceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string searchText = "old text"; 
+            string replaceText = "new text"; 
+            _textEditor.ReplaceText(searchText, replaceText);
+        }
+
+        private void FindToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string searchText = "search text"; 
+            _textEditor.Search(searchText);
+        }
+
+        private void FindNextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string searchText = "search text"; 
+            _textEditor.SearchNext(searchText);
+        }
+
+        private void FindPreviousToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string searchText = "search text"; 
+            _textEditor.SearchPrevious(searchText);
+        }
+
+        private void DateTimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _textEditor.InsertDateTime();
         }
     }
 }
