d6063ee131ef33756fec360747391e4a4fb49f94
olegveshchykov <ipz222_vom@student.ztu.edu.ua>
2024-05-19 16:24:03 +0300

    додано новий клас в бібліотеку класів Editing

--- Notepad/Form1.cs
+++ Notepad/Form1.cs
@@ -4,23 +4,71 @@ using System.Windows.Forms;
 using System.Drawing.Printing;
 using System.Diagnostics;
 using Microsoft.SqlServer.Server;
+using NotepadLibrary;
 
 namespace Notepad
 {
     public partial class Form1 : Form
     {
-
+        private FileManager _fileManager;
 
         public Form1()
         {
             InitializeComponent();
+            _fileManager = new FileManager(richTextBox1);
         }
+
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
             string text = richTextBox1.Text;
             string[] lines = text.Split('\n');
             Tekst.Text = text.Length.ToString();
             Simvol.Text = lines.Length.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void CreateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _fileManager.CreateNewFile();
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _fileManager.OpenFile();
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _fileManager.SaveFile();
+        }
+
+        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _fileManager.SaveFileAs();
+        }
+
+        private void PageSetupÑòîð³íêèToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _fileManager.PageSetup();
+        }
+
+        private void PrintToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _fileManager.Print();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NewWindowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 newForm = new Form1();
+            newForm.Show();
         }
     }
 }
---
+++ NotepadLibrary/Editing.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NotepadLibrary
+{
+    public class TextEditor
+    {
+        private TextBoxBase _textbox;
+
+        public TextEditor(TextBoxBase textbox)
+        {
+            _textbox = textbox ?? throw new ArgumentNullException(nameof(textbox));
+        }
+
+        public void Undo() => _textbox.Undo();
+
+        public void Cut() => _textbox.Cut();
+
+        public void Copy() => _textbox.Copy();
+
+        public void Paste() => _textbox.Paste();
+
+        public void Delete() => _textbox.SelectedText = string.Empty;
+
+        public void SelectAll() => _textbox.SelectAll();
+
+        public void ReplaceText(string searchText, string replaceText)
+        {
+            if (string.IsNullOrEmpty(searchText))
+            {
+                throw new ArgumentException("Search text cannot be null or empty.", nameof(searchText));
+            }
+
+            int index = FindText(searchText);
+            if (index != -1)
+            {
+                _textbox.Select(index, searchText.Length);
+                _textbox.SelectedText = replaceText;
+            }
+            else
+            {
+                MessageBox.Show("Text not found.", "Replace");
+            }
+        }
+
+        public void Search(string searchText)
+        {
+            if (string.IsNullOrEmpty(searchText))
+            {
+                throw new ArgumentException("Search text cannot be null or empty.", nameof(searchText));
+            }
+
+            int index = FindText(searchText);
+            if (index != -1)
+            {
+                _textbox.Select(index, searchText.Length);
+                _textbox.ScrollToCaret();
+            }
+            else
+            {
+                MessageBox.Show("Text not found.", "Search");
+            }
+        }
+
+        public void SearchNext(string searchText)
+        {
+            if (string.IsNullOrEmpty(searchText))
+            {
+                throw new ArgumentException("Search text cannot be null or empty.", nameof(searchText));
+            }
+
+            int currentPosition = _textbox.SelectionStart + _textbox.SelectionLength;
+            int index = _textbox.Text.IndexOf(searchText, currentPosition, StringComparison.CurrentCultureIgnoreCase);
+
+            if (index != -1)
+            {
+                _textbox.Select(index, searchText.Length);
+                _textbox.ScrollToCaret();
+            }
+            else
+            {
+                MessageBox.Show("Text not found.", "Search Next");
+            }
+        }
+
+        public void SearchPrevious(string searchText)
+        {
+            if (string.IsNullOrEmpty(searchText))
+            {
+                throw new ArgumentException("Search text cannot be null or empty.", nameof(searchText));
+            }
+
+            int currentPosition = _textbox.SelectionStart - 1;
+            int index = _textbox.Text.LastIndexOf(searchText, currentPosition, StringComparison.CurrentCultureIgnoreCase);
+
+            if (index != -1)
+            {
+                _textbox.Select(index, searchText.Length);
+                _textbox.ScrollToCaret();
+            }
+            else
+            {
+                MessageBox.Show("Text not found.", "Search Previous");
+            }
+        }
+
+        public void InsertDateTime() => _textbox.AppendText(DateTime.Now.ToString());
+
+        private int FindText(string searchText)
+        {
+            return _textbox.Text.IndexOf(searchText, StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
